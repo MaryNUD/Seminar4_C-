ccbde71a3b8267f28334fe08a06316d2e424de33
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-15 21:12:23 +0300

    modified seminar 4, task with array, sum p and o

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -7,8 +7,20 @@
     }
 }
 
+void SumArray(int[] array)
+{
+    int sum_p = 0, sum_n = 0;
+    for (int i = 0; i < array.Length; i++)
+        if(array[i] < 0)
+             sum_n += array[i];
+        else
+             sum_p += array[i];
+    Console.WriteLine($"Sum of positive numbers: {sum_p}; sum of negative numbers: {sum_n}.");
+}
+
 Console.Write("Insert the Length of an array: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 fillArray(array);
-Console.Write($"[{String.Join(", ", array)}]");
+Console.WriteLine($"[{String.Join(", ", array)}]");
+SumArray(array);
