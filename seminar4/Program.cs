5d82ee39e504913a2f95776bee48c20ac36ea008
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-15 19:44:13 +0300

    add seminar 4 Fill array and print

---
+++ seminar4/Program.cs
@@ -0,0 +1,14 @@
+﻿void fillArray(int[] array)
+{
+    int Length = array.Length;
+    for(int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(-15, 15);
+    }
+}
+
+Console.Write("Insert the Length of an array: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+fillArray(array);
+Console.Write($"[{String.Join(", ", array)}]");
