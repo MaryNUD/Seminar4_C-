83f21d54ce4ecdf2eb0a59bb61b284176510e9bb
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-17 14:55:16 +0300

    add new tasks in seminar 4

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -1,26 +1,86 @@
-﻿void fillArray(int[] array)
-{
-    int Length = array.Length;
-    for(int i = 0; i < Length; i++)
-    {
-        array[i] = new Random().Next(-15, 15);
-    }
-}
+﻿// void fillArray(int[] array)  // заполняем массив возврат не нужен!!!
+// {
+//     int Length = array.Length;
+//     for(int i = 0; i < Length; i++)
+//     {
+//         array[i] = new Random().Next(-15, 15);
+//     }
+// }
 
-void SumArray(int[] array)
-{
-    int sum_p = 0, sum_n = 0;
-    for (int i = 0; i < array.Length; i++)
-        if(array[i] < 0)
-             sum_n += array[i];
-        else
-             sum_p += array[i];
-    Console.WriteLine($"Sum of positive numbers: {sum_p}; sum of negative numbers: {sum_n}.");
-}
+// void SumArray(int[] array)
+// {
+//     int sum_p = 0, sum_n = 0;
+//     for (int i = 0; i < array.Length; i++)
+//         if(array[i] < 0)
+//              sum_n += array[i];
+//         else
+//              sum_p += array[i];
+//     Console.WriteLine($"Sum of positive numbers: {sum_p} \n sum of negative numbers: {sum_n}.");
+// }
 
-Console.Write("Insert the Length of an array: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[n];
-fillArray(array);
-Console.WriteLine($"[{String.Join(", ", array)}]");
-SumArray(array);
+// Console.Write("Insert the Length of an array: ");
+// int n = Convert.ToInt32(Console.ReadLine());      // the number of elements in an array
+// int[] array = new int[n];                  // create array with the inserted number of elemnts
+// fillArray(array);                         // call for thefunction, fill i the array
+// Console.WriteLine($"[{String.Join(", ", array)}]");   // make from array to string
+// SumArray(array);                //  call for the sum function 
+//..........................................................................................................
+
+
+// make them change their +/-
+// void fillArray(int[] array) 
+// {
+//     int Length = array.Length;
+//     for(int i = 0; i < Length; i++)
+//     {
+//         array[i] = new Random().Next(-15, 15);
+//     }
+// }
+
+// void Change(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//        array[i] *= (-1);
+//     Console.WriteLine($"[{String.Join(", ", array)}]"); 
+// }
+
+// Console.Write("Insert the Length of an array: ");
+// int n = Convert.ToInt32(Console.ReadLine());   
+// int[] array = new int[n];                  
+// fillArray(array);                         
+// Console.WriteLine($"[{String.Join(", ", array)}]");   
+// Change(array);                
+//......................................................................
+
+// find a certain number
+
+// void fillArray(int[] array) 
+// {
+//     int Length = array.Length;
+//     for(int i = 0; i < Length; i++)
+//     {
+//         array[i] = new Random().Next(-15, 15);
+//     }
+// }
+
+// string Find(int[] array, int f)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {   
+//         if(array[i] == f)
+//             return "yes";
+//     }
+//         return "no";
+    
+// }
+
+// Console.Write("Insert the Length of an array: ");
+// int n = Convert.ToInt32(Console.ReadLine());   
+// int[] array = new int[n];                  
+// fillArray(array);                         
+// Console.WriteLine($"[{String.Join(", ", array)}]");  
+// Console.Write("Insert the number you want to find: ");
+// int x = Convert.ToInt32(Console.ReadLine());   
+// Console.WriteLine(Find(array, x));              
+//.........................................................................................
+
